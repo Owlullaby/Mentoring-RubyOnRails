879e9d92cdc1c81361ed447f3b2eccd65ea2f6d8
owlullaby <alexwty626@gmail.com>
2022-01-15 09:35:46 +0800

    fix: remove changed directories

--- Section 1: Introduction and Setup/2-Let's code the quiz/main.rb
+++
@@ -1,9 +0,0 @@
-# create an array named "address"
-address = [1,2,3,4,5,6,7,8,9]
-# use p for print
-p address
-# print the value at index 5
-p address[5]
-# reverse the address array
-new_address = address.reverse!
-p new_address
