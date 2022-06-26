8afee88da91a9cf30d5ac6b6946d056411bea079
owlullaby <alexwty626@gmail.com>
2022-06-26 21:01:19 +0800

    fix: fix spacing in authenticator.rb

--- section2-the-ruby-programming-language/authenticator/authenticator.rb
+++ section2-the-ruby-programming-language/authenticator/authenticator.rb
@@ -3,8 +3,8 @@
 
 # create hash using symbol as keys
 users = [
-    { username: "Jack", password: "beanstalk"},
-    { username: "Marry", password: "littlelamb"}
+  { username: "Jack", password: "beanstalk"},
+  { username: "Marry", password: "littlelamb"}
 ]
 
 puts "Welcome to the authenticator"
@@ -15,31 +15,31 @@ puts "If password is correct, you will get back the user object"
 
 # authenticate function comparing username and password inputs with the hash
 def auth_user(username, password, users)
-    users.each do |user|
-        if user[:username] == username && user[:password] == password
-            puts "Welcome back, #{ username }!"
-            puts user
-            break
-        else
-            puts "Credentials were not correct"
-            break
-        end
+  users.each do |user|
+    if user[:username] == username && user[:password] == password
+      puts "Welcome back, #{ username }!"
+      puts user
+      break
+    else
+      puts "Credentials were not correct"
+      break
     end
+  end
 end
 
 # while loop for 3 attempts max
 attempts = 0
 # less than 3 because index starts at 0 so 0,1,2 = three times
 while attempts < 3
-    print "Username: "
-    username = gets.chomp
-    print "Password: "
-    password = gets.chomp
-    authentication = auth_user(username, password, users)
-    puts authentication
-    puts "Press n to quit or any other key to continue: "
-    input = gets.chomp.downcase
-    break if input == "n"
-    attempts += 1
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  authentication = auth_user(username, password, users)
+  puts authentication
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
 end
-puts "You have exceeded the number of attempts" if attempts == 3 
+puts "You have exceeded the number of attempts" if attempts == 3
